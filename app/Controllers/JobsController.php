2b16991b5cfd8172b99696e0eaee86f836cb07ef
jmanzo <jmanzosistemas@gmail.com>
2019-03-27 19:46:55 -0500

    respect/validator implementado para las validaciones

--- app/Controllers/JobsController.php
+++ app/Controllers/JobsController.php
@@ -3,19 +3,34 @@
 namespace App\Controllers;
 
 use App\Models\Job;
+use Respect\Validation\Validator as validator;
+use Respect\Validation\Exceptions\NestedValidationException;
 
 class JobsController extends BaseController
 {
 	public function getAddJob($request)
 	{
+		$jobValidator = validator::key('title', validator::stringType()->notEmpty())
+			->key('description', validator::stringType()->notEmpty());
+		$messageResponse = null;
+
 		if ($request->getMethod() == 'POST') {
 			$postData = $request->getParsedBody();
-		    $job = new Job();
-		    $job->title = $postData['title'];
-		    $job->description = $postData['description'];
-		    $job->save();
+
+			try {
+				$jobValidator->assert($postData);
+			    $job = new Job();
+			    $job->title = $postData['title'];
+			    $job->description = $postData['description'];
+			    $job->save();
+			    $messageResponse = 'Job Saved Successfully';
+			} catch (NestedValidationException $e) {
+				$messageResponse = $e->getMessage();
+			}
 		}
 
-		return $this->renderHTML('addJob.twig');
+		return $this->renderHTML('addJob.twig', [
+			'messageResponse' => $messageResponse,
+		]);
 	}
 }
